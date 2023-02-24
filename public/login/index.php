f1e61b42df82a5892034b116076e7adbcabe16a1
Suleiman <soleman630@gmail.com>
2023-02-24 23:01:13 +0200

    Updates

--- public/login/index.php
+++ public/login/index.php
@@ -47,21 +47,21 @@ require_once '../../include/page-head.php';
                         <div class="form-group">
                             <label>Email Address</label>
                             <div class="input-group"><span class="input-group-text">
-                                <i class="icon-email"></i></span>
+                                <i class="fa fa-at"></i></span>
                                 <input type="email" class="form-control" id="email" required="" placeholder="Test@gmail.com">
                             </div>
                         </div>
                         <div class="form-group">
                             <label>Password</label>
                             <div class="input-group"><span class="input-group-text">
-                                <i class="icon-lock"></i></span>
+                                <i class="fa fa-key"></i></span>
                                 <input type="password"class="form-control" id="password" required="" placeholder="*********">
                             </div>
                         </div>
                         <div class="form-group">
                             <label>PIN Code</label>
                             <div class="input-group"><span class="input-group-text">
-                                <i class="icon-key"></i></span>
+                                <i class="fa fa-map-pin"></i></span>
                                 <input type="text" class="form-control" id="pin-code" required="" placeholder="Your account Pin Code">
                             </div>
                         </div>
