58117afedb6dfe55e8bb6f8a7d43d28df0a8ea22
Suleiman <soleman630@gmail.com>
2023-02-25 01:44:26 +0200

    Updates

--- include/components/sidebar.php
+++ include/components/sidebar.php
@@ -1,6 +1,6 @@
 <header class="main-nav">
     <div class="sidebar-user text-center">
-        <img class="img-90 rounded-circle" src="../../assets/images/user/1.png" alt="">
+        <img class="img-90 rounded-circle" src="../../assets/images/user/user.png" alt="">
         <div class="badge-bottom"><span class="badge badge-primary">New</span></div>
         <h6 class="mt-3 f-14 f-w-600"><?php echo $session_username; ?></h6>
         <p class="mb-0 font-roboto"><?php echo $session_userEmail; ?></p>
